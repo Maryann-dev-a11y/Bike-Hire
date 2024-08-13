7416046949e92d8134cf777d2850d20a24b8f202
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-08-13 15:16:22 +0300

    Implementing  payment logic

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+
 const Navbar = () => {
-    return (
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <a href="#home">Bike Rental</a>
-        </div>
-        <ul className="navbar-links">
-          <li><a href="/">Home</a></li>
-          <li><a href="/bikes">Available Bikes</a></li>
-          <li><a href="# Rent now">Contact</a></li>
-          <li><a href="#about">About Us</a></li>
-        </ul>
-      </nav>
-    );
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedMode = localStorage.getItem('darkMode');
+    if (storedMode === 'true') {
+      setDarkMode(true);
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    localStorage.setItem('darkMode', !darkMode);
   };
-  
-  export default Navbar;
+
+  return (
+    <nav className={`navbar ${darkMode ? 'dark' : 'light'}`}>
+      <div className="navbar-logo">
+        <a href="#home">Bike Rental</a>
+      </div>
+      <ul className="navbar-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/bikes">Available Bikes</a></li>
+        <li><a href="#contact">Contact Us</a></li>
+        <li><a href="#about">About Us</a></li>
+      </ul>
+      <div className="mode-toggle">
+        <button onClick={toggleDarkMode} className="button">
+          {darkMode ? 'Light Mode' : 'Dark Mode'}
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 
 
 const paymentMethods = [
-  { id: 1, name: 'Visa', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvCh4Tcadq-f-WvsH7rXSGZgPKMZ826bo0Ew&s' },
-  { id: 2, name: 'MasterCard', image: 'mastercard.png' },
-  { id: 3, name: 'PayPal', image: 'paypal.png' },
-  { id: 4, name: 'Google Pay', image: 'google-pay.png' }
+  { id: 1, name: 'Visa', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-kIqA-9wBxUDDSp5W_P3xPIGxnTJJst3iMA&s' },
+  { id: 2, name: 'MasterCard', image: 'https://media.wired.com/photos/5926dea77034dc5f91bece36/master/w_1600%2Cc_limit/Mastercard3-1.jpg' },
+//   { id: 3, name: 'PayPal', image: 'paypal.png' },
+//   { id: 4, name: 'Google Pay', image: 'google-pay.png' }
 ];
 
 const PaymentOptions = () => {
